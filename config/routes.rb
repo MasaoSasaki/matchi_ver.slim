1411d7846af2162802af9dcac7de73157fece3fb
vagrant <vagrant@localhost.localdomain>
2020-08-09 03:48:18 +0000

    Update Controller master/authenticate & Add namespace 'owner'

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions: 'master/admins/sessions',
-    registrations: 'master/admins/registrations'
-  }
-  devise_for :restaurants, controllers: {
-    sessions: 'master/restaurants/sessions',
-    registrations: 'master/restaurants/registrations'
-  }
 
   devise_for :users
 
   namespace :master do
+    devise_for :admins, controllers: {
+      sessions: 'master/admins/sessions',
+      registrations: 'master/admins/registrations'
+    }
     get '/' => 'admins#top'
     resources :users
     resources :restaurants
   end
+
+  namespace :owner do
+    devise_for :restaurants, controllers: {
+      sessions: 'master/restaurants/sessions',
+      registrations: 'master/restaurants/registrations'
+    }
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
