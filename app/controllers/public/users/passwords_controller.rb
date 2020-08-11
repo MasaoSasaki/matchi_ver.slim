f2658250a2b5539a0de51d1fdfb624e06ccdab39
vagrant <vagrant@localhost.localdomain>
2020-08-11 10:54:57 +0000

    Fix Views header-login

--- app/controllers/public/users/passwords_controller.rb
+++ app/controllers/public/users/passwords_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Master::Restaurants::PasswordsController < Devise::PasswordsController
+class Public::Users::PasswordsController < Devise::PasswordsController
   # GET /resource/password/new
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-
   namespace :master do
     devise_for :admins, controllers: {
       sessions: 'master/admins/sessions',
@@ -22,6 +20,21 @@ Rails.application.routes.draw do
       resources :menus
     end
     resources :reservations
+  end
+
+  namespace :public do
+    devise_for :users, controllers: {
+      sessions: 'public/users/sessions',
+      registrations: 'public/users/registrations'
+    }
+    get '/' => 'homes#top'
+    get 'about' => 'homes#about'
+    resources :users do
+      resources :bookmarks
+    end
+    resources :reservations
+    resources :restaurants
+    resources :menus
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
