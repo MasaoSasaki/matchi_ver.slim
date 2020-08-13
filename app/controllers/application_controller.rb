35063cea0564e84bb6c9747ae3711d4b6b40f306
vagrant <vagrant@localhost.localdomain>
2020-08-13 03:12:55 +0000

    Update public/users/sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_user_permitted_parameters, if: :devise_controller?
+  # before_action :configure_restaurant_permitted_parameters, if: :devise_controller?
 
   protected
-  def configure_permitted_parameters
+  def configure_user_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family, :name_first, :name_family_kana, :name_first_kana, :phone_number])
   end
+
+  # def configure_restaurant_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family, :name_first, :name_family_kana, :name_first_kana, :phone_number])
+  # end
 
 end
