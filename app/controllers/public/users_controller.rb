647071d0ad09f3c945244ee34757a85f6e439a10
vagrant <vagrant@localhost.localdomain>
2020-08-13 04:09:59 +0000

    Update public/users/info

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,9 +1,11 @@
 class Public::UsersController < Public::Base
   def show
     @reservation = Reservation.where(user_id: params[:id])
+    @current_user = current_public_user
   end
 
   def info
+    @current_user = current_public_user
   end
 
   def edit
