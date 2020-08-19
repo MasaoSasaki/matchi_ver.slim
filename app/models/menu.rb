20e25dd2a14dda8541645fe38c2cd20462801978
vagrant <vagrant@localhost.localdomain>
2020-08-19 03:33:42 +0000

    Update owner/menus reservation_method

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu < ApplicationRecord
   belongs_to :restaurant
   has_many :reservation
 
-  enum reservation_status: [:予約のみ, :予約可, :予約不可]
+  enum reservation_method: {'ご予約のみ': 0, 'ご予約可能': 1, 'ご予約不可': 2}
 
   # attachment :menu_image
 end
