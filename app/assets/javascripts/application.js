c8084d7314353d79a61c5e70f3f36bbd56054d5b
vagrant <vagrant@localhost.localdomain>
2020-08-22 08:03:33 +0000

    Update sp-header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,28 @@
 //= require jquery.jpostal
 //= require_tree .
 
+//画面サイズによるCSSの処理
+const $win = $(window);
+
+$win.on('load resize', function() {
+  const windowWidth = window.innerWidth;
+
+  if (windowWidth <= 600) {
+    $(".top-main").children("div").toggleClass("flex");
+  }
+});
+
+
 //緯度経度から住所を取得
 $(function() {
-  $("#get-position").on("click", function() {
-    if (navigator.geolocation) {
-      // 現在地を取得
-      $(".location-result" + ".true").removeClass("hidden");
-      navigator.geolocation.getCurrentPosition(function(position) {
 
+  // top.html.erb(:1 JSONタグ)が読み込まれたら現在地を取得開始
+  $("#restaurants_json").ready(function() {
+    $("#loading").append("(読み込み中)");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
         const currentLat = position.coords.latitude;
         const currentLng = position.coords.longitude;
-        $("#latitude").text('緯度' + currentLat)
-        $("#longitude").text('経度' + currentLng)
-
         //現在地を地図上に表示
         $(function() {
           map = new google.maps.Map(document.getElementById("map"), {
@@ -72,17 +81,62 @@ $(function() {
                     );
                     marker[i].addListener("click", function(){
                       infoWindow[i].open(map, marker[i]);
-
                     });
+                  } else {
+                    $(".location-result" + ".error").removeClass("hedden");
                   }
                 }
               );
             }
           });
         });
+        $("#loading").remove();
       });
     } else {
       $(".location-result" + ".false").removeClass("hidden");
+      // レストラン情報を地図上に表示
+      const restaurants = gon.restaurants;
+      $(function(){
+        for (let i = 0; i < restaurants.length; i++) {
+          let address = restaurants[i].prefecture + restaurants[i].city + restaurants[i].street;
+          const geocoder = new google.maps.Geocoder();
+          geocoder.geocode(
+            {"address": address},
+            function(result, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                var lat = result[0].geometry.location.lat();
+                var lng = result[0].geometry.location.lng();
+                marker[i] = new google.maps.Marker({
+                  position: new google.maps.LatLng(lat, lng),
+                  map: map
+                });
+                i = i + 1;
+                var link = "https://matchi-gourmet/public/restaurants/"+i;
+                i = i - 1;
+                infoWindow[i] = new google.maps.InfoWindow(
+                  {
+                    content: restaurants[i].name + '<br><a href='+link+'>詳細</a>',
+                  },
+                );
+                marker[i].addListener("click", function(){
+                  infoWindow[i].open(map, marker[i]);
+                });
+              } else {
+                $(".location-result" + ".error").removeClass("hedden");
+              }
+            }
+          );
+        }
+      });
+      $("#loading").remove();
     }
   });
+
+  // geolocationが使えるかどうかの処理（ウインドウ）
+  $("#get-position").on("click", function() {
+    if (navigator.geolocation) {
+      $(".location-result" + ".true").removeClass("hidden");
+    }
+  });
+
 });
