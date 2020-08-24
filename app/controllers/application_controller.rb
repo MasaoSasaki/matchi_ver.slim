5ed86c4bede8ce3aa0eea1a9f5803bd6fcfbb2e9
vagrant <vagrant@localhost.localdomain>
2020-08-24 07:21:58 +0000

    Fix users/registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_user_permitted_parameters, if: :devise_controller?
+
   protected
   def configure_user_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family, :name_first, :name_family_kana, :name_first_kana, :phone_number])
