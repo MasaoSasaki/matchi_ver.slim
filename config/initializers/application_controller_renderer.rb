f8bd4ba1823f1a0385a57e24a0825f517ea83a42
vagrant <vagrant@localhost.localdomain>
2020-08-25 06:21:17 +0000

    Update profile_image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#refile用シークレットキー
+Refile.secret_key = '9fc9b68057bbff697dbd02fc6bd1229ab8650c3c3707da223cb46037601b4e74763646f96cf3cd1426263bf8ed59a0f3cefe1ca4acaa9ac241fae2d0c6265875'
