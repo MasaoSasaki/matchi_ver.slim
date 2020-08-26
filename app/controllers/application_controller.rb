e7a18da1e2c098971e5b48dcdf922e1cc6c297e5
vagrant <vagrant@localhost.localdomain>
2020-08-26 15:13:23 +0000

    Update user-access-limit close #52

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_user_permitted_parameters, if: :devise_controller?
 
+  def current_user?
+    user_id = params[:user_id]
+    id = params[:id]
+    unless user_id == current_public_user.id.to_s || id == current_public_user.id.to_s
+      id = "#{params[:user_id]}" + "#{params[:id]}"
+      redirect_to redirect_path(user_id: id)  #redirect先に入力されたIDを渡す
+    end
+  end
+
   protected
   def configure_user_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name_family, :name_first, :name_family_kana, :name_first_kana, :phone_number])
--- app/controllers/public/base.rb
+++ app/controllers/public/base.rb
@@ -1,3 +1,4 @@
 class Public::Base < ApplicationController
   before_action :authenticate_public_user!, except: [:top, :about, :contact, :privacy, :terms, :admin]
+  before_action :current_user?, except: [:top, :about, :admin, :contact, :privacy, :terms, :redirect]
 end
