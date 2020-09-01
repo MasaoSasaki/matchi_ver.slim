490f49f5ebc25e458d769a41819b73f5002c8e02
vagrant <vagrant@localhost.localdomain>
2020-09-01 06:41:48 +0000

    Add views master/tags#index

--- app/controllers/master/tags_controller.rb
+++ app/controllers/master/tags_controller.rb
@@ -1,15 +1,26 @@
-class Master::TagsController < Mater::Base
+class Master::TagsController < Master::Base
 
   def index
-
+    @tags = Tag.all
   end
 
   def create
-
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to action: 'index'
+    else
+      render 'index'
+    end
   end
 
   def destroy
+    Tag.find(params[:id]).destroy
+    redirect_to action: 'index'
+  end
 
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 
 end
