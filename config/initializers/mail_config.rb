c1f6eff10d93aa91168821b3a3aafeb830bc709a
vagrant <vagrant@localhost.localdomain>
2020-09-06 06:11:55 +0000

    Fix mail_config typo

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
   domain: 'gmail.com',
   port: 587,
   user_name: ENV['GMAIL_USERNAME'],
-  password: ENV['GMAIL_PASSWAORD'],
+  password: ENV['GMAIL_PASSWORD'],
   authentication: 'plain',
   enable_starttls_auto: true
 }
