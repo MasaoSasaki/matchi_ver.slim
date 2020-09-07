29fec5f00e9e526177d5b3ed3851cccf2920b4b5
vagrant <vagrant@localhost.localdomain>
2020-09-07 13:23:12 +0000

    Update mastre/restaurants#index remove :show

--- app/controllers/master/restaurants_controller.rb
+++ app/controllers/master/restaurants_controller.rb
@@ -1,14 +1,10 @@
 class Master::RestaurantsController < Master::Base
 
-  before_action :exist?, onli: [:show]
+  before_action :exist?, only: [:show]
 
   def index
     @restaurants = Restaurant.all
     @menus = Menu.all
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-  
 end
